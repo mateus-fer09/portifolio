78d04963cd07b028fea4b12c3b402cbdc302cf3a
Mateus Oliveira <mateussfer09@gmail.com>
2022-11-18 16:26:57 -0300

    Modificação

---
+++ mateus-oliveira/JS/menu.js
@@ -0,0 +1,8 @@
+function clickmenu(){
+    var link = document.getElementById('links')
+    if (link.style.display === 'block') {
+        link.style.display = 'none'
+    } else {
+        link.style.display = 'block'
+    }
+}
