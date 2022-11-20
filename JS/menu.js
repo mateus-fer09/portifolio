1d491d223737e7ae0e56354542981715d667cb99
Mateus Oliveira <mateussfer09@gmail.com>
2022-11-20 12:36:58 -0300

    Alteração no Menu

--- JS/menu.js
+++ JS/menu.js
@@ -1,8 +1,13 @@
-function clickmenu(){
-    var link = document.getElementById('links')
-    if (link.style.display === 'block') {
-        link.style.display = 'none'
-    } else {
-        link.style.display = 'block'
-    }
-}
+/* Set the width of the side navigation to 250px and the left margin of the page content to 250px and add a black background color to body */
+function openNav() {
+    document.getElementById("mySidenav").style.width = "250px";
+    document.getElementById("main").style.marginLeft = "250px";
+    document.body.style.backgroundColor = "rgba(0,0,0,0.4)";
+  }
+  
+  /* Set the width of the side navigation to 0 and the left margin of the page content to 0, and the background color of body to white */
+  function closeNav() {
+    document.getElementById("mySidenav").style.width = "0";
+    document.getElementById("main").style.marginLeft = "0";
+    document.body.style.backgroundColor = "white";
+  }
