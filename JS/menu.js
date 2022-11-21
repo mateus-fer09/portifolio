16d13ea85310a07cad366b4770b99cdffd8c09e3
Mateus Oliveira <mateussfer09@gmail.com>
2022-11-20 23:46:15 -0300

    alteração de estilo

--- JS/menu.js
+++ JS/menu.js
@@ -1,6 +1,6 @@
 /* Set the width of the side navigation to 250px and the left margin of the page content to 250px and add a black background color to body */
 function openNav() {
-    document.getElementById("mySidenav").style.width = "250px";
+    document.getElementById("mySidenav").style.width = "100%";
     document.getElementById("main").style.marginLeft = "250px";
     document.body.style.backgroundColor = "rgba(0,0,0,0.4)";
   }
